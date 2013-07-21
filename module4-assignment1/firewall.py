ca4edc7ca7f2fa320212c26665a302e49241ffcb
Akeel Ali <agilowen@gmail.com>
2013-07-20 19:36:48 -0700

    Module 4 correct submission

--- module4-assignment1/firewall.py
+++ module4-assignment1/firewall.py
@@ -14,14 +14,13 @@ from pox.lib.util import dpidToStr
 from pox.lib.addresses import EthAddr
 from collections import namedtuple
 import os
-''' Add your imports here ... '''
+import csv
 
 
 
 log = core.getLogger()
 policyFile = "%s/pox/pox/misc/firewall-policies.csv" % os.environ[ 'HOME' ]  
 
-''' Add your global variables here ... '''
 
 
 
@@ -32,9 +31,15 @@ class Firewall (EventMixin):
         log.debug("Enabling Firewall Module")
 
     def _handle_ConnectionUp (self, event):    
-        ''' Add your logic here ... '''
-        
-
+    	csv_file = csv.DictReader(open(policyFile, 'rb'), delimiter=',', quotechar='"')
+ 	
+	for line in csv_file:
+		msg = of.ofp_flow_mod()
+		msg.priority = 65535
+		msg.match.dl_src = EthAddr(line['mac_0'])
+		msg.match.dl_dst = EthAddr(line['mac_1'])
+		event.connection.send(msg)
+		log.debug("Dropping packets from %s to %s", line['mac_0'], line['mac_1'])
     
         log.debug("Firewall rules installed on %s", dpidToStr(event.dpid))
 
